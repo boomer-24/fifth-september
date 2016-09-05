8d11dad1e8f8538e02f500263645cc557c2deffd
boomer <boomer-24@yandex.ru>
2016-09-05 23:26:19 +0300

    sixth commit

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,6 @@
-#include "mainwindow.h"
+﻿#include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+//anything else note
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui_(new Ui::MainWindow)
