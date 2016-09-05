1d9ab68296a8c06927370f40092960b9bae78458
boomer <boomer-24@yandex.ru>
2016-09-05 21:05:44 +0300

    fourth commit

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-//something comment
-//new
+//something comment // nothingelse matter
+
     return a.exec();
 }
